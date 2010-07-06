d7114d07326a16579eb02e470d0c6ad3773c3e70
dmccuan <dmccuan@2d608090-4f55-0410-8acf-4111cef86056>
2010-07-06 16:27:19 +0000

    Correct fortran for complex nmbrs

--- blopex_petsc/driver_fortran/blopex_c.c
+++ blopex_petsc/driver_fortran/blopex_c.c
@@ -1,3 +1,9 @@
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+/* @@@ BLOPEX (version 1.1) LGPL Version 2.1 or above.See www.gnu.org. */
+/* @@@ Copyright 2010 BLOPEX team http://code.google.com/p/blopex/     */
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
+
 /*****************************************************************************
 
 Example of using BLOPEX with PETSc from Fortran.
