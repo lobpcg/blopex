8ca9fa1bf4c2dffce5b6bf20202a97572e63882a
dmccuan <dmccuan@2d608090-4f55-0410-8acf-4111cef86056>
2011-08-25 20:14:37 +0000

    remove blopex_petsc directory

--- blopex_petsc/Petsc32to64.m
+++
@@ -1,9 +0,0 @@
-function Petsc32to64(dir,file,comp)
-% example >>
-% Petsc32to64('C:/BLOPEX/blopex_petsc/driver_fiedler/','L-matrix','double')
-% 
-namein=[dir file '.petsc']; 
-A=PetscBinaryRead(namein,'int32',comp);
-nameout=[dir file '_64.petsc'];
-PetscBinaryWrite(nameout,'int64',A); 
-end
