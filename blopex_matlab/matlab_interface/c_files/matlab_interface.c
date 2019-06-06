33b5e9f7b9de3a9c28edbc0e22381f3f2edce315
Jose E. Roman <jroman@dsic.upv.es>
2019-06-06 19:08:07 +0200

    Change to MIT / Apache-2.0 License

--- blopex_matlab/matlab_interface/c_files/matlab_interface.c
+++ blopex_matlab/matlab_interface/c_files/matlab_interface.c
@@ -1,6 +1,6 @@
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
-/* @@@ BLOPEX (version 1.1) LGPL Version 2.1 or above.See www.gnu.org. */
-/* @@@ Copyright 2010 BLOPEX team http://code.google.com/p/blopex/     */
+/* @@@ BLOPEX (version 1.2) MIT / Apache-2.0 License                   */
+/* @@@ Copyright 2010-2019 Team https://github.com/lobpcg/blopex       */
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 
 /* this file contains interface routines to matlab for blopex */
--- blopex_serial_double/driver/serial_driver.c
+++ blopex_serial_double/driver/serial_driver.c
@@ -1,6 +1,6 @@
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
-/* @@@ BLOPEX (version 1.1) LGPL Version 2.1 or above.See www.gnu.org. */
-/* @@@ Copyright 2010 BLOPEX team http://code.google.com/p/blopex/     */
+/* @@@ BLOPEX (version 1.2) MIT / Apache-2.0 License                   */
+/* @@@ Copyright 2010-2019 Team https://github.com/lobpcg/blopex       */
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 /* This code was developed by Merico Argentati, Andrew Knyazev, Ilya Lashuk and Evgueni Ovtchinnikov */
 /* test driver for double serial implementation of lobpcg */
